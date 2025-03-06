b7a4d6a804f88e229af3a00022c8c32309c966cd
yontanhabtmu45 <bhabtamu163@gmail.com>
2025-03-06 18:14:36 +0300

    Update Weather.js
    last

--- OneDrive/Desktop/Weather App/weather-app/src/components/Weather.js
+++ OneDrive/Desktop/Weather App/weather-app/src/components/Weather.js
@@ -1,26 +1,29 @@
 import React, { useEffect, useRef, useState } from "react";
 import "./Weather.css";
-import searchIcon from "../assets/search-26.png";
-import cloudIcon from "../assets/cloud-96.png";
-import drizzleIcon from "../assets/drizzle-96.png";
-import humidityIcon from "../assets/humidity.gif";
-import rainIcon from "../assets/rain-cloud-96.png";
-import snowIcon from "../assets/snow-96.png";
-import sunnyIcon from "../assets/sunny-96.png";
-import windIcon from "../assets/wind.gif";
+import searchIcon from "../assets/Icons/clouds/search-26.png";
+import fewCloud from "../assets/Icons/clouds/few-cloud.png";
+import cloudIcon from "../assets/Icons/clouds/cloud-96.png";
+import drizzleIcon from "../assets/Icons/clouds/drizzle-96.png";
+import humidityIcon from "../assets/Icons/clouds/humidity.gif";
+import rainIcon from "../assets/Icons/clouds/rain-cloud-96.png";
+import snowIcon from "../assets/Icons/clouds/snow-96.png";
+import sunnyIcon from "../assets/Icons/clouds/sunny-96.png";
+import windIcon from "../assets/Icons/clouds/wind.gif";
+import maxTemp from "../assets/Icons/Temp/max-temperature.png";
+import minTemp from "../assets/Icons/Temp/min-temperature.png";
+import airDir from "../assets/Icons/arrows/weather-station-wind.gif"
+import barometer from "../assets/Icons/arrows/barometer.gif"
 
 function Weather() {
   const inputRef = useRef();
 
   const [WeatherData, setWeatherData] = useState(false);
-  const [cityData, setCityData] = useState("");
-
 
   const allIcons = {
     "01d": sunnyIcon,
     "01n": sunnyIcon,
-    "02d": cloudIcon,
-    "02n": cloudIcon,
+    "02d": fewCloud,
+    "02n": fewCloud,
     "03d": cloudIcon,
     "03n": cloudIcon,
     "04d": drizzleIcon,
@@ -34,26 +37,6 @@ function Weather() {
   };
 
 
-  //   const location = async () => {
-
-  // if()
-
-  //     try {
-  //       const locationUrl = "https://ipinfo.io/json?token=bbeb1f03094873";
-  //       let responseUrl = await fetch(locationUrl);
-  //       let dataUrl = await responseUrl.json();
-  //       // console.log(dataUrl);
-
-  //       setCityData({
-  //         cityName: dataUrl.city,
-  //         countryName: dataUrl.country,
-  //       });
-  //     } catch (error) {
-  //     //   console.error("Error:", error);
-  //     }
-  //   };
-  //   location();
-
   const search = async (city) => {
     if (city === "") {
       alert("Please enter city name");
@@ -66,19 +49,26 @@ function Weather() {
       const response = await fetch(url);
       const data = await response.json();
 
+      
+
       if (!response.ok) {
         alert(data.message);
         return;
       }
 
-      //  console.log(data);
+      console.log(data);
       const icon = allIcons[data.weather[0].icon] || sunnyIcon;
       setWeatherData({
         humidity: data.main.humidity,
         windSpeed: data.wind.speed,
+        windDeg: data.wind.deg,
         temprature: Math.floor(data.main.temp),
+        maxTemp: data.main.temp_max,
+        minTemp: data.main.temp_min,
         location: data.name,
         icon: icon,
+        feels: data.main.feels_like,
+        pressure: data.main.pressure,
       });
     } catch (error) {
       setWeatherData(false);
@@ -91,99 +81,100 @@ function Weather() {
   }, []);
 
   return (
-    <div className="weather">
-      <div className="search-box">
-        <input ref={inputRef} type="text" placeholder="Search City" />
-        <img
-          src={searchIcon}
-          alt=""
-          onClick={() => search(inputRef.current.value)}
-        />
-      </div>
+    <div className="wrapper">
+      <div className="weather">
+        <div className="search-box">
+          <input ref={inputRef} type="text" placeholder="Search City" 
+                      onKeyDown={(e) => {
+                        if (e.key === "Enter")
+                          search(inputRef.current.value)
+                        }}
+          />
+          <img
+            src={searchIcon}
+            alt=""
+            onClick={() => search(inputRef.current.value)}
+          />
+        </div>
 
-      <div className="city-address">
-        <p>
-          Current Location: {cityData.cityName}, {cityData.countryName}
-        </p>
-      </div>
 
-      {WeatherData ? (
-        <>
-          <br />
-          <img src={WeatherData.icon} alt="" className="weather-icon" />
-          <p className="temprature">{WeatherData.temprature}°C</p>
-          <p className="location">{WeatherData.location}</p>
+        {WeatherData ? (
+          <>
+            <br />
+            <img src={WeatherData.icon} alt="" className="weather-icon" />
+            <p className="temprature">{WeatherData.temprature}°C</p>
+            <p className="location">{WeatherData.location}</p>
 
-          <div className="weather-data">
-            <div className="col">
-              <img src={humidityIcon} alt="" />
-              <div>
-                <p>{WeatherData.humidity} %</p>
-                <span>Humidity</span>
+            <div className="weather-data">
+              <div className="col">
+                <img src={humidityIcon} alt="" />
+                <div>
+                  <p>{WeatherData.humidity} %</p>
+                  <span>Humidity</span>
+                </div>
+              </div>
+              <div className="col">
+                <img src={windIcon} alt="" />
+                <div>
+                  <p>{WeatherData.windSpeed} km/h</p>
+                  <span>Wind Speed</span>
+                </div>
               </div>
             </div>
-            <div className="col">
-              <img src={windIcon} alt="" />
-              <div>
-                <p>{WeatherData.windSpeed} km/h</p>
-                <span>Wind Speed</span>
+          </>
+        ) : (
+          <></>
+        )}
+      </div>
+      <div className="daily-info">
+        <div className="today">
+          <div className="sessions">
+            FEELS LIKE <br />
+            <div className="info">
+              <div className="weather-info">
+                <img src={WeatherData.icon} alt="" />
               </div>
+              <div className="weather-info">{WeatherData.feels}°C</div>
             </div>
           </div>
+          <div className="sessions">
+            MAX TEMPERATURE <br />
+            <div className="info">
+              <div className="weather-info">
+                <img src={maxTemp} alt="" />
+              </div>
+              <div className="weather-info">{WeatherData.maxTemp}°C</div>
+            </div>
+          </div>
+          <div className="sessions">
+            MIN TEMPERATURE <br />
+            <div className="info">
+              <div className="weather-info">
+                <img src={minTemp} alt="" />
+              </div>
+              <div className="weather-info">{WeatherData.minTemp}°C</div>
+            </div>
+          </div>
+        </div>
+        
+        <div className="prediction">
+          <div className="yesterday">
+            WIND DIRECTION <br /> <br />
+            <div className="inner-temp">
+              <img src={airDir} alt="" />
+            </div>
+              <div className="temp">{WeatherData.windDeg}°</div>
+          </div>
+          <div className="tomorrow">
+            PRESSURE <br /> <br />
+            <div className="inner-temp">
+              <img src={barometer} alt="" />
+            </div>
+              <div className="temp">{WeatherData.pressure}hPa</div>
+          </div>
+        </div>
 
-          <p className="prefered-dress">Prefered Clothes</p>
-          <div className="clothes">
-            <div className="for-men">
-              <b>For Men:</b> <br />
-              <p> <br /> <br />
-                In sunny days:  <br/>
-                "Lightweight Shirts", <br />
-                "Shorts or Chinos", <br />
-                "Sunglasses"
-              </p> <br/> <br/>
-              <p>
-                In cloudy days: <br />
-                "Light Jacket or Sweater", <br />
-                "Long-Sleeve Shirt", <br />
-                "Jeans or Chinos"
-              </p> <br/> <br/>
-              <p>
-                In cloudy days: <br />
-                "Waterproof Jacket", <br />
-                "Layered Clothing", <br />
-                "Waterproof Pants", <br />
-              </p>
-            </div>
-            <div className="for-women">
-              <b>For Women:</b> <br />
-              <p>
-                <br /> <br/>
-                In sunny days: <br/>
-                "Sundresses", <br />
-                "Skirts or Shorts", <br />
-                "Tank Tops or Blouses"
-              </p>{" "}
-              <br />
-              <br />
-              <p>
-                In cloudy days: <br /> 
-                "Cardigan or Light Jacket", <br />
-                "Long-Sleeve Top", <br />
-                "Jeans, Leggings, or Skirt"
-              </p>
-                <br /> <br/>
-              <p>
-                In rainny days: <br/>
-                "Raincoat or Trench Coat", <br />
-                "Waterproof Pants or Leggings", <br />
-                "Jeans, Leggings, or Skirt"
-              </p>
-            </div>
-          </div>
-        </>
-      ) : (
-        <></>
-      )}
+      </div>
     </div>
   );
 }
